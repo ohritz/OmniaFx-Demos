7a6eb11ea9d85f550a1e3a043a15acd04b041fe5
Sohan Fernando <sohan@graceful.nu>
2018-01-25 13:08:05 +0100

    create a pfx of the pems, for use in DevelopmentHosting

--- src/Frontend/Omnia.Fx.Demo/Resources/environment.manifest.ts
+++ src/Frontend/Omnia.Fx.Demo/Resources/environment.manifest.ts
@@ -1,12 +1,12 @@
-﻿import {ensureCertificate} from '@microsoft/gulp-core-build-serve/lib/certificates';
-import { Composer, DevelopmentEnvironment} from '@omnia/fx/tooling';
-import {Configuration} from 'webpack';
-import {PFX_PASSWORD} from '../tools/configs.consts';
+﻿import { Composer, DevelopmentEnvironment} from '@omnia/fx/tooling';
+import {pfxPassphrase, pfxPath} from '../tools/configs.consts';
+
+const pfxPath = './tools/certs/certificate.pfx';
 
 DevelopmentEnvironment.hosting
     .use({
         portNumber: 569,
         https: true,
-        pfxPath: './tools/certificate.pfx',
-        pfxPassphrase: PFX_PASSWORD,
+        pfxPath,
+        pfxPassphrase,
     });
--- src/Frontend/Omnia.Fx.Demo/gulpfile.js
+++ src/Frontend/Omnia.Fx.Demo/gulpfile.js
@@ -1,10 +1,45 @@
 require("./TaskRunner/Tasks/index.js");
-const gulp = require('gulp');
-
+const gulp = require("gulp");
+const pem = require("pem");
+// const path = require("path");
+const { writeFileSync } = require("fs");
+const {pfxPassphrase, pfxPath, certPath, keyPath} = require("./tools/configs.consts");
+const CertificateStore = require("@microsoft/gulp-core-build-serve/lib/CertificateStore")
+  .default;
 const {
   trustDevCert,
   untrustDevCert
 } = require("@microsoft/gulp-core-build-serve");
 
-gulp.task("trust-cert", cb => trustDevCert.executeTask(gulp, cb));
+const rootPath = process.cwd();
+// const pfxPath = path.join(rootPath, "tools", "certificate.pfx");
+// const keyPath = path.join(rootPath, "tools", "key.pem");
+// const certPath = path.join(rootPath, "tools", "cert.pem");
+
+gulp.task("trust-cert", cb => {
+  trustDevCert.executeTask(gulp, () => {
+    const certStore = CertificateStore.instance;
+    const { cert, key } = {
+      cert: certStore.certificateData,
+      key: certStore.keyData
+    };
+    writeFileSync(keyPath, key);
+    writeFileSync(certPath, cert);
+
+    pem.convert.PEM2PFX(
+      {
+        cert: certPath,
+        key: keyPath
+      },
+      pfxPath,
+      pfxPassphrase,
+      (err, result) => {
+        if (!err){ 
+         return cb();
+        }
+        return console.error(err);
+      }
+    );
+  });
+});
 gulp.task("untrust-cert", cb => untrustDevCert.executeTask(gulp, cb));
